0fd0bcf6304997d8e38b4be43df40743d41c3419
yhkim <yhkim@doublechain.co.kr>
2019-01-24 12:01:53 +0900

    Modify google drive file link error.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // In the main process.
-const {app, BrowserWindow} = require('electron')
+const {app, shell, BrowserWindow} = require('electron')
 
 // Or use `remote` from the renderer process.
 // const { BrowserWindow } = require('electron').remote
@@ -16,6 +16,31 @@ function createWindow () {
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+
+  // new-window
+  // will-navigate
+  // did-navigate-in-page
+  mainWindow.webContents.on('new-window', (event, url) => {
+    event.preventDefault()
+    let newWin = new BrowserWindow({show: false})
+
+    newWin.webContents.on('did-navigate-in-page', (nextEvent, nextUrl) => {
+      let currentURL = newWin.webContents.getURL()
+      // console.log('nextUrl : '+nextUrl)
+      // console.log('currentURL : '+currentURL)
+      if(!currentURL.startsWith('https://www.docswave.com/') && !currentURL.startsWith('https://accounts.google.com/')) {
+        nextEvent.preventDefault()
+        shell.openExternal(currentURL)
+        newWin.close()
+      }
+    });
+
+    newWin.once('ready-to-show', () => newWin.show())
+    newWin.loadURL(url)
+    // newWin.webContents.openDevTools()
+    event.newGuest = newWin
+
+  });
 }
 
 app.on('ready', createWindow)
